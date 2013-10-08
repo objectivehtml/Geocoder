d51d8c4c79bd8d42639a45ed20cd0b3aadd4ec3e
Justin Kimbrell <justin@objectivehtml.com>
2013-10-08 14:34:34 -0400

    final updates

--- system/gmap_geocoder/config/gmap_geocoder_config.php
+++ system/gmap_geocoder/config/gmap_geocoder_config.php
@@ -2,7 +2,7 @@
 
 if(!defined('GMAP_GEOCODER_VERSION'))
 {
-	define('GMAP_GEOCODER_VERSION', '1.0.9.1');
+	define('GMAP_GEOCODER_VERSION', '1.1.0');
 }
 
 $config['gmap_geocoder_version'] = GMAP_GEOCODER_VERSION;
